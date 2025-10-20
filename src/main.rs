b17f1bba0dae7586e7d72e19d191632bdbf115b1
Kai Michaelis <seu@das-labor.org>
2025-10-20 23:12:33 +0800

    add --locked to cargo build

--- src/main.rs
+++ src/main.rs
@@ -30,13 +30,13 @@ mod config;
 use config::{Cli, Configuration, Input};
 
 mod arrow;
-pub use crate::arrow::{TickBuilder, TokenBuilder};
+use crate::arrow::{TickBuilder, TokenBuilder};
 
 mod pump;
-pub use crate::pump::{CreateEvent, TradeEvent};
+use crate::pump::{CreateEvent, TradeEvent};
 
 mod solana;
-pub use crate::solana::notify_block;
+use crate::solana::notify_block;
 
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
