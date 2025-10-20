6fa2a2b18970e709b0c3d80de1f6233b068ec550
Kai Michaelis <seu@das-labor.org>
2025-10-20 22:26:39 +0800

    support remote checkpoint

--- src/config.rs
+++ src/config.rs
@@ -1,13 +1,14 @@
-use std::path::{Path, PathBuf, absolute};
+use std::ffi::OsString;
+use std::path::Path;
 
 use clap;
 use log::info;
+use object_store::ObjectStore;
 use serde::{Deserialize, Serialize};
-use tokio::fs;
 use toml;
 use url::Url;
 
-use crate::Result;
+use crate::{Result, open_store};
 
 const DEFAULT_CHECKPOINT_FILE: &str = "checkpoint.toml";
 
@@ -19,7 +20,7 @@ pub struct Cli {
         value_name = "FILE",
         default_value = DEFAULT_CHECKPOINT_FILE
     )]
-    pub checkpoint_file: PathBuf,
+    pub checkpoint_file: String,
 
     #[arg(long = "data-dir", value_name = "DIR", default_value = "data")]
     pub data_dir: String,
@@ -62,7 +63,7 @@ pub enum Input {
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct Configuration {
     pub data_dir: Url,
-    pub checkpoint_file: PathBuf,
+    pub checkpoint_file: Url,
     pub input: Input,
     pub tick_queue_size: usize,
     pub token_queue_size: usize,
@@ -77,28 +78,17 @@ impl Configuration {
         } else if let Some(epoch) = cli.input.epoch {
             Input::Remote { epoch, offset: 0 }
         } else if cli.input.resume {
-            return Ok(Self::from_file(&cli.checkpoint_file).await?);
+            return Ok(Self::from_checkpoint(&cli.checkpoint_file).await?);
         } else {
             return Err("Either --local-file, --epoch or --resume must be specified".into());
         };
 
-        let data_dir = match Url::parse(&cli.data_dir) {
-            Ok(url) => url,
-            Err(url::ParseError::RelativeUrlWithoutBase) => {
-                let p = absolute(Path::new(&cli.data_dir))?;
-                Url::from_directory_path(p).map_err(|err| {
-                    format!(
-                        "Failed to parse data directory as URL or valid path: {:?}",
-                        err
-                    )
-                })?
-            }
-            Err(err) => return Err(err.into()),
-        };
+        let data_dir = to_url(&cli.data_dir)?;
+        let checkpoint_file = to_url(&cli.checkpoint_file)?;
 
         Ok(Self {
             data_dir,
-            checkpoint_file: cli.checkpoint_file,
+            checkpoint_file,
             tick_queue_size: cli.tick_queue_size,
             token_queue_size: cli.token_queue_size,
             tick_batch_size: cli.tick_batch_size,
@@ -107,11 +97,40 @@ impl Configuration {
         })
     }
 
-    pub async fn from_file<P: AsRef<Path>>(path: P) -> Result<Self> {
-        let c = fs::read_to_string(path).await?;
-        let s = toml::from_str(&c)?;
+    pub async fn from_checkpoint<S: AsRef<str>>(s: S) -> Result<Self> {
+        let u = to_url(s.as_ref())?;
+
+        let p = Path::new(u.path());
+        let dir = p.parent().unwrap_or_else(|| &Path::new("/"));
+        let file = p
+            .file_name()
+            .map(|f| f.to_owned())
+            .unwrap_or_else(|| OsString::from(DEFAULT_CHECKPOINT_FILE));
+
+        let mut base = u.clone();
+        base.set_path(dir.to_str().ok_or("Invalid UTF-8 in path")?);
+        let store = open_store(&base)?;
+
+        let c = store
+            .get(&file.to_str().ok_or("Invalid UTF-8 in path")?.into())
+            .await?;
+        let s = toml::from_slice(&c.bytes().await?)?;
+
         info!("Loaded checkpoint: {:?}", s);
         Ok(s)
+    }
+}
+
+fn to_url(path: &str) -> Result<Url> {
+    match Url::parse(path) {
+        Ok(url) => Ok(url),
+        Err(url::ParseError::RelativeUrlWithoutBase) => {
+            let p = Path::new(path).canonicalize()?;
+            Ok(Url::from_file_path(p).map_err(|err| {
+                format!("Failed to parse path as URL or valid file path: {:?}", err)
+            })?)
+        }
+        Err(err) => Err(err.into()),
     }
 }
 
@@ -123,14 +142,37 @@ impl Configuration {
         }
     }
 
+    pub async fn remove_checkpoint(&self) -> Result<()> {
+        let mut base = self.checkpoint_file.clone();
+        base.set_path("");
+        let store = open_store(&base)?;
+
+        store
+            .delete(&self.checkpoint_file.path().to_string().into())
+            .await?;
+
+        Ok(())
+    }
+
     pub async fn checkpoint(&self, offset: u64) -> Result<()> {
         let mut s = self.clone();
         match &mut s.input {
             Input::Remote { offset: o, .. } => *o = offset,
             Input::Local { offset: o, .. } => *o = offset,
         }
+
+        let mut base = self.checkpoint_file.clone();
+        base.set_path("");
+        let store = open_store(&base)?;
+
         let c = toml::to_string(&s)?;
-        fs::write(&self.checkpoint_file, c).await?;
+        store
+            .put(
+                &self.checkpoint_file.path().into(),
+                c.as_bytes().to_owned().into(),
+            )
+            .await?;
+
         Ok(())
     }
 }
