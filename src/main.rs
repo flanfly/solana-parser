56ef9b473c8a45bf15299d5ab35a88e682dee737
Kai Michaelis <seu@das-labor.org>
2025-10-20 11:27:46 +0800

    remove bench, merge lib

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,14 @@ use log::{error, info, warn};
 mod config;
 use config::{Cli, Configuration, Input};
 
-use solana_parser::{CreateEvent, TickBuilder, TokenBuilder, TradeEvent, notify_block};
+mod arrow;
+pub use crate::arrow::{TickBuilder, TokenBuilder};
+
+mod pump;
+pub use crate::pump::{CreateEvent, TradeEvent};
+
+mod solana;
+pub use crate::solana::notify_block;
 
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
